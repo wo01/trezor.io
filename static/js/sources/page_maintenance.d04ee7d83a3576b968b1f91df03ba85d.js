4e46adc86433cc9ba13073317f0586ee2e9193a2
chren <peter.privalinec@gmail.com>
2017-01-20 18:05:54 +0100

    added start page

---
+++ static/js/sources/page_maintenance.d04ee7d83a3576b968b1f91df03ba85d.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var a=requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;a&&a(function(){$("img.lazy").lazyload({threshold:500})});$("#maintenance").css("min-height",window.innerHeight-280+"px")});
