ce264739e9a0490c73371c795da70e961b1f0dcd
Peter Jensen <peter.privalinec@gmail.com>
2017-01-24 15:54:37 +0100

    updates in error and footer

--- static/js/sources/page_error.818bc32c99aa58c9b88c782b2f34fded.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#url").text(window.location.pathname);
-    var a = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-    a && a(function () {
-        $("img.lazy").lazyload({threshold: 500})
-    });
-    $("#error").css("min-height", window.innerHeight - 280 + "px")
-});
