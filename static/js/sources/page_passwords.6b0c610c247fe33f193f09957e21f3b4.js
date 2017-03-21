039edc7635631fa9c7b194d9afb322cfc418fbcc
Peter Jensen <tuximail@gmail.com>
2017-03-21 17:21:48 +0100

    added functions page and minor visual details

---
+++ static/js/sources/page_passwords.6b0c610c247fe33f193f09957e21f3b4.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var a=requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;a&&a(function(){$("img.lazy").lazyload({threshold:500})})});
