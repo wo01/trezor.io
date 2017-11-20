e8c6a56cb3c8335ccdb241b9136159e14c7c4e83
fantamyroslav <myroslav.fanta@satoshilabs.com>
2017-11-20 14:52:45 +0100

    QR Codes added with javascript, URI added to QR Codes, Modal fixed

---
+++ static/js/qrcodes/customcodes.js
@@ -0,0 +1,17 @@
+var btc = new QRCode("qrcode-btc", {
+    text: "3QmuBaZrJNCxc5Xs7aGzZUK8RirUT8jRKf",
+    width: 250,
+    height: 250,
+});
+
+var ltc = new QRCode("qrcode-ltc", {
+    text: "MCwFeGBkgpqN1vU4Wgs2kWhcFb3Qz6S592",
+    width: 250,
+    height: 250,
+});
+
+var eth = new QRCode("qrcode-eth", {
+    text: "0x140E5C635D484f2AAA153d2B7d4c7A945242c21D",
+    width: 250,
+    height: 250,
+});
