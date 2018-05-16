8e26b068d0daa1ce657087c7dca63db986da27f2
Peter Jensen <tuximail@gmail.com>
2018-05-16 16:36:10 +0200

    update respinsive table on desktop

--- static/js/sources/page_homepage.js
+++ static/js/sources/page_homepage.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
     var target = this.hash,
       $target = $(target);
     $('html, body').stop().animate({
-      scrollTop: $target.offset().top + 2
+      scrollTop: $target.offset().top + 20
     }, 500, 'swing', function () {
       window.location.hash = target;
     });
