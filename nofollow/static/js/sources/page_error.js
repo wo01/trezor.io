9f85f68138c5ea626da51a59d946eb7d427a96d5
Peter Jensen <tuximail@gmail.com>
2018-05-28 11:13:31 +0200

    update UI interface

--- nofollow/static/js/sources/page_error.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    var $error = $('#error'),
-        h = window.innerHeight - 280;
-    // height fixer
-    $error.css('min-height', h + 'px');
-});
--- nofollow/static/js/sources/page_maintenance.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-  var $maintenance = $('#maintenance'),
-    h = window.innerHeight - 280;
-  // height fixer
-  $maintenance.css('min-height', h + 'px');
-})
